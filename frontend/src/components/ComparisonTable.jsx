f4397f41d782925c52a74cf62c202db12e4bff5e
Joel Kinny <joelkny97@gmail.com>
2025-07-08 01:43:24 -0400

    Add full stack project skeleton

---
+++ frontend/src/components/ComparisonTable.jsx
@@ -0,0 +1,17 @@
+function ComparisonTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Plan</th>
+          <th>Category</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* rows go here */}
+      </tbody>
+    </table>
+  );
+}
+export default ComparisonTable;
